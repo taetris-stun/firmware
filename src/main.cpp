62b67395357bed9f3719d3e57e56a4421a97601e
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-06-10 00:54:31 +0200

    PLAYABLE :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
-
+#include <DNSServer.h>
+#include <WebSocketsClient.h>
 #include <ESP_DoubleResetDetector.h>
-#define RESET_TIMEOUT 2000
+
+#define AP_PASSWORD "tinkertoe"
+#define SERVER_ADDRESS "taetris-stun.tinkertoe.net"
+#define SERVER_PORT 3001
+#define SHOCKER_ID "4ta82s"
+#define TRIGGER_PIN 14
+#define RESET_TIMEOUT 1000
 #define RESET_ADDRESS 0
 
+WebSocketsClient webSocket;
+int shockTime = 0;
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println();
-  Serial.println("[ dispo-shocker // v0.0.1 ]");
-  Serial.println();
-
+void setupWifi() {
   WiFiManager wifiManager;
   wifiManager.setDebugOutput(false);
 
@@ -35,11 +37,56 @@ void setup() {
   resetDetector->stop();
 
   Serial.println("- starting WiFiManager");
-  wifiManager.autoConnect("dispo-shocker", "tinkertoe");
+  wifiManager.autoConnect("dispo-shocker", AP_PASSWORD);
 
   Serial.println("- connected to network");
 }
 
-void loop() {
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch (type)
+  {
+    case WStype_DISCONNECTED:
+      Serial.println("- disconnected from server");
+      break;
 
+    case WStype_CONNECTED:
+      Serial.println("- connected to server");
+      webSocket.sendTXT(SHOCKER_ID);
+      break;
+
+    case WStype_TEXT:
+      Serial.println("- received shock command");
+      Serial.println(String((char *)payload));
+      shockTime = String((char *)payload).toInt();
+      break;
+    
+    default:
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println();
+  Serial.println("[ dispo-shocker // v0.0.1 ]");
+  Serial.println();
+
+  setupWifi();
+  
+  pinMode(TRIGGER_PIN, OUTPUT);
+
+  webSocket.begin(SERVER_ADDRESS, SERVER_PORT, "/");
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
+}
+
+void loop() {
+  webSocket.loop();
+  if (shockTime > 0) {
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delay(shockTime);
+    digitalWrite(TRIGGER_PIN, LOW);
+    shockTime = 0;
+  }
 }
